e636a2f70ae4e29921e811808227521ea3de913e
Masahiro Aikawa <aikawame@wor.jp>
2018-09-04 18:57:44 +0900

    Integrate files

--- app.js
+++
@@ -1,8 +0,0 @@
-const getUrlParam = targetKey => {
-  try {
-    const keyValue = location.search.slice(1).split('&').map(v => v.split('=')).filter(v => v[0] === targetKey)[0];
-    return decodeURIComponent(keyValue[1]);
-  } catch {
-    return undefined;
-  }
-};
